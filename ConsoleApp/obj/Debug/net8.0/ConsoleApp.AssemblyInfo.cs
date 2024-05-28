e40272ec002a7271b694d613dfec980732c0028f
hongquan <danghongquanvtc1111@gmail.com>
2024-05-28 09:22:21 +0700

    update

--- ConsoleApp/obj/Debug/net8.0/ConsoleApp.AssemblyInfo.cs
+++ ConsoleApp/obj/Debug/net8.0/ConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42a383bcf3d5bb0c5568f97d3c4284a4c1e349ea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b32e4978815378afd9fe33e8a689a37ecba70725")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
