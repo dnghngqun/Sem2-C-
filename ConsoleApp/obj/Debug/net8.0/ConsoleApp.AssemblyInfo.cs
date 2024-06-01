3f87c27609ddaa9c54f41f88e96aac8fb4360657
hongquan <danghongquanvtc1111@gmail.com>
2024-06-01 08:47:14 +0700

    update

--- ConsoleApp/obj/Debug/net8.0/ConsoleApp.AssemblyInfo.cs
+++ ConsoleApp/obj/Debug/net8.0/ConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c121c96a9c277bb4a064e59d3110eb5441e06c09")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f1b638e886fc8a003cf15f164e05e01161b3ff9")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Pro3/Program.cs
+++ Pro3/Program.cs
@@ -13,7 +13,9 @@ namespace Pro3
                 Console.WriteLine("Product Management");
                 Console.WriteLine("1. Add product"); 
                 Console.WriteLine("2. Display all product");
-                Console.WriteLine("3. Exit");
+                Console.WriteLine("3. Update product");
+                Console.WriteLine("4. Delete product");
+                Console.WriteLine("5. Exit");
                 Console.Write("Enter choice: ");
                 string choice = Console.ReadLine();
 
@@ -26,6 +28,12 @@ namespace Pro3
                         DisplayAllProduct();
                         break;
                     case "3":
+                        UpdateProduct();
+                        break;
+                    case "4":
+                        DeleteProduct();
+                        break;
+                    case "5":
                         return;
                     default:
                         Console.WriteLine(choice + " is not a valid choice");
@@ -58,6 +66,52 @@ namespace Pro3
                 }
                  
             }
+        }
+
+        static void UpdateProduct()
+        {
+            Product product = new Product();
+            Console.Write("Enter product id to update: ");
+            product.Id = int.Parse(Console.ReadLine());
+            Console.Write("Enter product name: ");
+            product.Name = Console.ReadLine();
+            Console.Write("Enter product price: ");
+            product.Price = Convert.ToDecimal(Console.ReadLine());
+            Console.Write("Enter product description: ");
+            product.Description = Console.ReadLine();
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                conn.Open();
+                string Query = "UPDATE Products SET name = '@Name', price = @Price, Description = '@Description' WHERE id = @Id";
+                using(MySqlCommand cmd = new MySqlCommand(Query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@Id", product.Id);
+                    cmd.Parameters.AddWithValue("@Name", product.Name);
+                    cmd.Parameters.AddWithValue("@Price", product.Price);
+                    cmd.Parameters.AddWithValue("@Description", product.Description);
+                    cmd.ExecuteNonQuery();
+                    Console.WriteLine("Product updated successfully"); 
+                }
+;
+            }
+        }
+
+        static void DeleteProduct(){
+            Console.Write("Enter product id to delete: ");
+            int Id = int.Parse(Console.ReadLine());
+            using(MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                conn.Open();
+                string Query = "DELETE Products WHERE id = @Id";
+                using(MySqlCommand cmd = new MySqlCommand(Query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@Id", Id);
+                    cmd.ExecuteNonQuery();
+                    Console.WriteLine("Product removed succcessfully!");
+                }
+            }
+
+
         }
 
         static void DisplayAllProduct()
