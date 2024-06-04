04c686af42094f9fd05709aaf3dd2f2da98ade71
hongquan <danghongquanvtc1111@gmail.com>
2024-06-04 12:04:50 +0700

    up

--- Pro3/service/OrderService.cs
+++ Pro3/service/OrderService.cs
@@ -19,7 +19,28 @@ namespace Pro3.service
         }
 
         public void AddOrder(Order order){
-
+            conn.Open();
+            MySqlCommand cmd = conn.CreateCommand();
+            //create new giao dich
+            MySqlTransaction transaction = conn.BeginTransaction();
+            //thuc hien code them don hang moi
+            //...
+            cmd.CommandText = "INSERT INTO orders(customer_id, order_date) values(@CustomerId, @OrderDate)";
+            cmd.Parameters.AddWithValue("@CustomerId",order.CustomerId);
+            cmd.Parameters.AddWithValue("@OrderDate", order.OrderDate);
+            cmd.ExecuteNonQuery();
+            int OrderId = (int)cmd.LastInsertedId;// lay ra id moi nhat vua duoc add o tren
+            foreach(var detail in order.OrderDetails)
+            {
+                cmd.CommandText = "INSERT INTO order_detals(orderId,productId,quantity) VALUES(@OrderId, @ProductID, @Quantity)";
+                cmd.Parameters.AddWithValue("@OrderId", OrderId);
+                cmd.Parameters.AddWithValue("@ProductId", detail.ProductId);
+                cmd.Parameters.AddWithValue("@Quantity", detail.Quantity);
+                cmd.ExecuteNonQuery();
+            
+            }
+            //ket thuc giao dich
+            transaction.Commit();
         }
         public List<Order> GetAllOrders(){
             return null;
